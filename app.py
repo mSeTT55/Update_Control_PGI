d9785f0957257f5ad6233cdb27d53cc5ecee6377
Mateus Sena Almeida <86931085+mSeTT55@users.noreply.github.com>
2025-05-15 14:08:48 -0300

    feat: add connection SSH and screen with button to updates

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def index():
     return render_template("index.html", output=output)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=3000, debug=True)
--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 # config.py
 
-# ——————— CONFIGURAÇÃO SSH ———————
+# —————— CONFIGURAÇÃO SSH ——————
 SSH_HOST     = "10.148.62.10"             # Host ou IP do servidor PGI
 SSH_USER     = "pgi.service"                     # Usuário SSH
 SSH_KEY_PATH = "C:/Users/mateussena/OneDrive - OR/Documentos/.ssh/pgi.service"  # Caminho da sua chave privada
 
-# ——————— PATH DO SCRIPT ———————
+# —————— PATH DO SCRIPT ——————
 SCRIPT_PATH  = "/dados/.pgi-update.sh"    # Script que será executado
